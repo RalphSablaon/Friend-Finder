877e1d2886fd98037c73a4d7d04fd2edfc90ca69
Ralph Sablaon <rsablaon88@gmail.com>
2019-09-20 16:34:29 -0700

    added sample friend data

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+        name: "Pennywise",
+        profilePic: "https://cdn.vox-cdn.com/thumbor/7TksO3CYNwjreyiI_AsMd74G57I=/0x0:1382x598/1200x800/filters:focal(581x189:801x409)/cdn.vox-cdn.com/uploads/chorus_image/image/65195967/Screen_Shot_2019_05_09_at_12.27.45_PM.0.jpg",
+        scores: [
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
+
+module.exports = friendList;
